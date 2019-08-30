3e5e1bf6371c2285605aacd49983a22feefc7789
gasseklopper <m-morley@gmx.de>
2019-08-31 01:32:53 +0200

    addeed dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,26 @@
-const gulp = require('gulp')
-const sourcemaps = require('gulp-sourcemaps')
-const browserify = require('browserify')
+//initialize Modules
+const { src, dest, series, parallel, watch} = require('gulp')
+// Scss Modules
+const autoprefixer = require('gulp-autoprefixer')
 const babelify = require('babelify')
-const source = require('vinyl-source-stream')
+const browserify = require('browserify')
 const buffer = require('vinyl-buffer')
-const uglify = require('gulp-uglify')
-
-const sass = require('gulp-sass')
-const browserSync = require('browser-sync').create()
 const cleancss = require('gulp-clean-css')
 const csscomb = require('gulp-csscomb')
-const rename = require('gulp-rename')
-const autoprefixer = require('gulp-autoprefixer')
+const cssnano = require('gulp-cssnano')
 const pug = require('gulp-pug')
+const postcss = require('gulp-postcss')
+const rename = require('gulp-rename')
+const replace = require('gulp-replace')
+const sourcemaps = require('gulp-sourcemaps')
+const source = require('vinyl-source-stream')
+const sass = require('gulp-sass')
+const uglify = require('gulp-uglify')
 
+//brsowser-sync Modules
+const browserSync = require('browser-sync').create()
+
+// File Path variables
 const paths = {
   _js: './assets/js/**/*.js',
   doc: './docs/src/scss/*.scss',
@@ -23,30 +30,28 @@ const paths = {
   styleDist:  './dist/assets/css',
   styleSrc:  './assets/scss/**/*.scss',
 }
-
+// Javascript source array
 const jsFILES = [paths.jsSRC]
 
-
-// compile scss into css for browsersync workflow
-function style() {
-	// 1. wheree is my scss file
-	return gulp.src(paths.styleWatch)
-	.pipe( sourcemaps.init() )
-	// 2. pass that file trogh my sass compiler
-	.pipe(sass({outputStyle: 'compact', precision: 10}).on('error', sass.logError))
-	.pipe(autoprefixer({
+//Sass Task
+function scssTask(done){
+	return src(paths.styleWatch)
+		.pipe(sourcemaps.init() )
+		.pipe(sass({outputStyle: 'compact', precision: 10}).on('error', sass.logError))
+		.pipe(autoprefixer({
 			cascade: false
 		}))
-	.pipe(csscomb())
-	.pipe( sourcemaps.write( './'))
-	// 3. where do I save the compiled CSS?
-	.pipe(gulp.dest(paths.styleDist))
-	// 4. stream changes to all browsers
-	.pipe(browserSync.stream())
-
+		.pipe(csscomb())
+		.pipe(sourcemaps.write( './'))
+		.pipe(dest(paths.styleDist))
+		.pipe(cleancss())
+		.pipe(rename({suffix: '.min'}))
+		.pipe(sourcemaps.write( './'))
+		.pipe(dest(paths.styleDist))
+		done()
 }
 
-function js() {
+function jsTask(done){
 	jsFILES.map(function( entry ){
 		return browserify({
 			entries: [entry]
@@ -59,49 +64,27 @@ function js() {
 		.pipe( sourcemaps.init({ loadMaps: true }))
 		.pipe( uglify())
 		.pipe( sourcemaps.write( './'))
-		.pipe(gulp.dest(paths.jsDIST))
-
+		.pipe(dest(paths.jsDIST))
 	})
+	done()
 }
 
-// compile scss into css and minified for production
-function build() {
-	// 1. wheree is my scss file
-	return gulp.src(paths.styleSrc)
-	// 2. Init sourcemap
-	.pipe( sourcemaps.init() )
-	// 3. pass that file trough my sass compiler
-	.pipe(sass({outputStyle: 'compact', precision: 10}).on('error', sass.logError))
-	// 3a. use Prefixer
-	.pipe(autoprefixer({
-			cascade: false
-		}))
-	.pipe(csscomb())
-	// 3. where do I save the compiled CSS?
-	.pipe( sourcemaps.write( './'))
-	.pipe(gulp.dest(paths.styleDist))
-	.pipe(cleancss())
-	.pipe(rename({
-		suffix: '.min'
-	}))
-	// 4. where do I save the minified CSS?
-	.pipe( sourcemaps.write( './'))
-	.pipe(gulp.dest(paths.styleDist))
-
-}
-
-function watch() {
+function start(done) {
 	browserSync.init({
 		server: {
 			baseDir: './'
 		}
 	})
-	gulp.watch(paths.styleWatch, style)
-	gulp.watch('./*.html').on('change', browserSync.reload)
-	gulp.watch('./assets/js/**/*.js').on('change', browserSync.reload)
+	watch(paths.styleWatch, scssTask)
+	watch('./*.html').on('change', browserSync.reload)
+	watch('./assets/js/**/*.js').on('change', browserSync.reload)
+	done()
 }
 
-exports.build = build
-exports.js = js
-exports.style = style
-exports.watch = watch
+exports.default = series(
+	parallel(scssTask, jsTask)
+)
+
+exports.scssTask = scssTask
+exports.jsTask = jsTask
+exports.start = start
