7716611f4838abe748d8ee77b6f78a1b4f7483c8
gasseklopper <m-morley@gmx.de>
2019-09-01 10:37:42 +0200

    cleaned up

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,38 @@
-//initialize Modules
+//initialize task Modules
 const { src, dest, series, parallel, watch} = require('gulp')
-// Scss Modules
-const autoprefixer = require('gulp-autoprefixer')
-const babelify = require('babelify')
-const browserify = require('browserify')
-const buffer = require('vinyl-buffer')
-const cleancss = require('gulp-clean-css')
-const csscomb = require('gulp-csscomb')
-const cssnano = require('gulp-cssnano')
-const pug = require('gulp-pug')
-const postcss = require('gulp-postcss')
-const rename = require('gulp-rename')
-const replace = require('gulp-replace')
-const sourcemaps = require('gulp-sourcemaps')
-const source = require('vinyl-source-stream')
-const sass = require('gulp-sass')
-const uglify = require('gulp-uglify')
-const malvid = require('malvid')
-const pAll = require('p-all')
-const util = require('util')
-const fs = require('fs')
-const nunjucksRender = require('gulp-nunjucks-render')
-const data = require('gulp-data')
+// initialize  Modules
+const autoprefixer 		= require('gulp-autoprefixer')
+const babelify 			= require('babelify')
+const browserify 		= require('browserify')
+const buffer 			= require('vinyl-buffer')
+const cleancss 			= require('gulp-clean-css')
+const csscomb 			= require('gulp-csscomb')
+const cssnano 			= require('gulp-cssnano')
+const data 				= require('gulp-data')
+const fs 				= require('fs')
+const malvid 			= require('malvid')
+const nunjucksRender 	= require('gulp-nunjucks-render')
+const pAll 				= require('p-all')
+const pug 				= require('gulp-pug')
+const postcss 			= require('gulp-postcss')
+const rename 			= require('gulp-rename')
+const replace 			= require('gulp-replace')
+const sourcemaps 		= require('gulp-sourcemaps')
+const source 			= require('vinyl-source-stream')
+const sass 				= require('gulp-sass')
+const uglify 			= require('gulp-uglify')
+const util 				= require('util')
 
 //browser-sync Modules
 const browserSync = require('browser-sync').create()
 
 // File Path variables
 const paths = {
-  _js: './assets/js/**/*.js',
-  doc: './docs/src/scss/*.scss',
-  jsSRC: './assets/js/script.js',
-  jsDIST: './dist',
-  styleWatch:  './assets/scss/**/*.scss',
-  styleDist:  './dist/assets/css',
-  styleSrc:  './assets/scss/**/*.scss',
-  nunjucksWatch: './app/**/*',
+	jsSRC: './assets/js/script.js',
+	jsDIST: './dist',
+	styleWatch:  './assets/scss/**/*.scss',
+	styleDist:  './dist/assets/css',
+	nunjucksWatch: './app/**/*',
 }
 // Javascript source array
 const jsFILES = [paths.jsSRC]
@@ -58,6 +55,7 @@ function scssTask(done){
 		done()
 }
 
+//Js Task
 function jsTask(done){
 	jsFILES.map(function( entry ){
 		return browserify({
@@ -76,9 +74,10 @@ function jsTask(done){
 	done()
 }
 
+// Async Malvid UI Task
 async function malvidTask(done){
 	const results = await malvid({
-		src: './src'
+		src: './app'
 	})
 	const html = await results.html()
 	const json = await results.json()
@@ -104,30 +103,27 @@ function nunjucksTask(){
 		.pipe(dest('dist'))
 }
 
+// Watch Tasks
 function start(done) {
+	console.log('Start watching...')
 	browserSync.init({
-		server: {
-			baseDir: './dist'
-		}
+		server: {baseDir: './dist'}
 	})
 	watch(paths.styleWatch, scssTask)
 	watch(paths.nunjucksWatch, nunjucksTask)
-	watch('src/**/*.html').on('change', browserSync.reload)
-	watch('src/index.html').on('change', browserSync.reload)
-	watch('src/index.html.json').on('change', browserSync.reload)
-	watch('./*.html').on('change', browserSync.reload)
 	watch('./assets/js/**/*.js').on('change', browserSync.reload)
 	watch('./app/pages/**/*.+(html|njk)').on('change', browserSync.reload)
 	watch('./app/templates/**/*.+(html|njk)').on('change', browserSync.reload)
-
 	done()
 }
 
+// Run default Task 'gulp'
 exports.default = series(
 	parallel(scssTask, jsTask),
 	start
 )
 
+// Run Tasks 'gulp scssTask', 'gulp jsTask'...
 exports.scssTask = scssTask
 exports.jsTask = jsTask
 exports.malvidTask = malvidTask
