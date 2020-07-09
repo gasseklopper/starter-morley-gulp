bedce12740ceb7e6012b4de14b8b27a43896da80
gasseklopper <m-morley@gmx.de>
2020-07-09 19:49:43 +0200

    Revert "Revert "added a lot""
    This reverts commit 4440ae889699ba12d9fe8959a74486258257598f.

---
+++ src/assets/js/modules/paralax_paintings.js
@@ -0,0 +1,28 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+
+gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollToPlugin);
+
+
+
+console.clear();
+console.log('test')
+
+
+class Paintinks {
+	constructor() {
+   var  select = e => document.querySelector(e);
+   var  selectAll = e => document.querySelectorAll(e);
+
+    const stage = select('.stage')
+    console.log('test satge', stage);
+
+
+
+   console.info('Paintinks initiddafafasdasdlized')
+	}
+}
+
+export default Paintinks
--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,28 +1,49 @@
-import App from './modules/app.js'
-import Nav from './modules/nav.js'
+// import App from './modules/app.js'
+// import Nav from './modules/nav.js'
 import Paralax from './modules/paralax.js'
-// import barba from '@barba/core';
-// import { gsap } from "gsap";
+import Paintinks from './modules/paralax_paintings.js'
+import barba from '@barba/core';
+import { gsap } from "gsap";
 // import { MotionPathPlugin } from "gsap/MotionPathPlugin";
 // import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 // gsap.registerPlugin(MotionPathPlugin, ScrollTrigger, MorphSVGPlugin);
-// barba.init({
-//         transitions: [{
-//           name: 'opacity-transition',
-//           leave(data) {
-//             return gsap.to(data.current.container, {
-//               opacity: 0
-//             });
-//           },
-//           enter(data) {
-//             return gsap.from(data.next.container, {
-//               opacity: 0
-//             });
-//           }
-//         }]
-//       });
+var navigation = gsap.timeline({paused:true, reversed:true});
+navigation.to("#navigationWrap", 0.5, {opacity: 1, display: 'block'})
+          .to(".navbar", 0.3, {opacity: 0}, "-=0.1")
+          .to(".close", 0.3, {display: "block", opacity: 1}, "-=0.1")
+          .from(".menu", 0.5, {opacity: 0, y: 30})
+var menu = document.querySelector('.navbar')
+var close = document.querySelector('.close')
 
-const app = new App()
-const nav = new Nav()
+menu.onclick  = () => {
+console.log('navbar', menu)
+navigation.reversed() ? navigation.play() : navigation.reverse();
+}
+close.onclick  = () => {
+console.log('navbar close', menu)
+navigation.reversed() ? navigation.play() : navigation.reverse();
+}
+// $(".navbar, .close").click (function() {
+//   navigation.reversed() ? navigation.play() : navigation.reverse();
+// })
+barba.init({
+        transitions: [{
+          name: 'opacity-transition',
+          leave(data) {
+            return gsap.to(data.current.container, {
+              opacity: 0
+            });
+          },
+          enter(data) {
+            return gsap.from(data.next.container, {
+              opacity: 0
+            });
+          }
+        }]
+      });
+
+// const app = new App()
+// const nav = new Nav()
 const paralax = new Paralax()
+const paintinks = new Paintinks()
