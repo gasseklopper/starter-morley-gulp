494ffbede2d015de63058db01ae15b833c5ff40b
gasseklopper <m-morley@gmx.de>
2020-07-10 13:08:42 +0200

    added cursor and cleaned up

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -2,6 +2,8 @@
 // import Nav from './modules/nav.js'
 import Paralax from './modules/paralax.js'
 import Paintinks from './modules/paralax_paintings.js'
+// import Cursor from './modules/cursor.js'
+import Cursor from './modules/cursor3';
 import barba from '@barba/core';
 import { gsap } from "gsap";
 // import { MotionPathPlugin } from "gsap/MotionPathPlugin";
@@ -27,23 +29,17 @@ navigation.reversed() ? navigation.play() : navigation.reverse();
 // $(".navbar, .close").click (function() {
 //   navigation.reversed() ? navigation.play() : navigation.reverse();
 // })
-barba.init({
-        transitions: [{
-          name: 'opacity-transition',
-          leave(data) {
-            return gsap.to(data.current.container, {
-              opacity: 0
-            });
-          },
-          enter(data) {
-            return gsap.from(data.next.container, {
-              opacity: 0
-            });
-          }
-        }]
-      });
 
+
+
+const cursor = new Cursor(document.querySelector('.cursor'));
+
+[...document.querySelectorAll('a')].forEach(el => {
+    el.addEventListener('mouseenter', () => cursor.emit('enter'));
+    el.addEventListener('mouseleave', () => cursor.emit('leave'));
+});
 // const app = new App()
 // const nav = new Nav()
 const paralax = new Paralax()
+// const cursor = new Cursor(document.querySelector('.cursor'));
 const paintinks = new Paintinks()
---
+++ src/assets/js/utils/utils.js
@@ -0,0 +1,54 @@
+// // Map number x from range [a, b] to [c, d]
+// const map = (x, a, b, c, d) => (x - a) * (d - c) / (b - a) + c;
+
+// // Linear interpolation
+// const lerp = (a, b, n) => (1 - n) * a + n * b;
+
+// const clamp = (num, min, max) => num <= min ? min : num >= max ? max : num;
+
+// // Gets the mouse position
+// const getMousePos = (e) => {
+//     let posx = 0;
+//     let posy = 0;
+//     if (!e) e = window.event;
+//     if (e.pageX || e.pageY) {
+//         posx = e.clientX;
+//         posy = e.clientY;
+//     }
+// 	// console.log('clientX', e.clientX)
+// 	// console.log('clientY', e.clientY)
+// 	// console.log('mouse position x -> ', posx)
+// 	// console.log('mouse position y -> ', posy)
+//     return { x : posx, y : posy }
+// };
+
+// // Generate a random float.
+// const getRandomFloat = (min, max) => (Math.random() * (max - min) + min).toFixed(2);
+
+// export { map, lerp, clamp, getMousePos, getRandomFloat };
+
+
+// Map number x from range [a, b] to [c, d]
+const map = (x, a, b, c, d) => (x - a) * (d - c) / (b - a) + c;
+
+// Linear interpolation
+const lerp = (a, b, n) => (1 - n) * a + n * b;
+
+const calcWinsize = () => {
+    return {width: window.innerWidth, height: window.innerHeight};
+};
+
+// Gets the mouse position
+const getMousePos = (e) => {
+    let posx = 0;
+    let posy = 0;
+    if (!e) e = window.event;
+    if (e.pageX || e.pageY) {
+        posx = e.clientX;
+        posy = e.clientY;
+    }
+
+    return { x : posx, y : posy }
+};
+
+export { map, lerp, calcWinsize, getMousePos };
