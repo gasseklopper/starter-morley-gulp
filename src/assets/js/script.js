4371e6a843679b4ed5b5928fda87b688e3e9c7d7
gasseklopper <m-morley@gmx.de>
2020-08-09 00:10:07 +0200

    added home

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -5,23 +5,23 @@ import Paintinks from './modules/paralax_paintings.js'
 
 import Cursor from './modules/cursor3'
 
-const navigation = gsap.timeline({ paused: true, reversed: true })
-navigation
-	.to('#navigationWrap', 0.5, { opacity: 1, display: 'block' })
-	.to('.navbar', 0.3, { opacity: 0 }, '-=0.1')
-	.to('.close', 0.3, { display: 'block', opacity: 1 }, '-=0.1')
-	.from('.menu', 0.5, { opacity: 0, y: 30 })
-const menu = document.querySelector('.navbar')
-const close = document.querySelector('.close')
+// const navigation = gsap.timeline({ paused: true, reversed: true })
+// navigation
+// 	.to('#navigationWrap', 0.5, { opacity: 1, display: 'block' })
+// 	.to('.navbar', 0.3, { opacity: 0 }, '-=0.1')
+// 	.to('.close', 0.3, { display: 'block', opacity: 1 }, '-=0.1')
+// 	.from('.menu', 0.5, { opacity: 0, y: 30 })
+// const menu = document.querySelector('.navbar')
+// const close = document.querySelector('.close')
 
-menu.onclick = () => {
-	console.log('navbar', menu)
-	navigation.reversed() ? navigation.play() : navigation.reverse()
-}
-close.onclick = () => {
-	console.log('navbar close', menu)
-	navigation.reversed() ? navigation.play() : navigation.reverse()
-}
+// menu.onclick = () => {
+// 	console.log('navbar', menu)
+// 	navigation.reversed() ? navigation.play() : navigation.reverse()
+// }
+// close.onclick = () => {
+// 	console.log('navbar close', menu)
+// 	navigation.reversed() ? navigation.play() : navigation.reverse()
+// }
 // $(".navbar, .close").click (function() {
 //   navigation.reversed() ? navigation.play() : navigation.reverse();
 // })
